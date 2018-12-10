7d9d2ef4d16c820a9b43aed4e988c6f9ee67cb1b
sandralosan <45710660+sandralosan@users.noreply.github.com>
2018-12-10 17:09:14 +0530

    csrf changes

--- masonhering003.js
+++ masonhering003.js
@@ -1,20 +1,19 @@
-var department,departmentRes,csrf;
+var csrf;
 var cookie=document.cookie.split(';');
 for(i=0;i<cookie.length;i++)
 {
-  if((cookie[i]).startsWith(" CT_CSRF")==true)
+  if((cookie[i]).startsWith(" crmcsr")==true)
   {
-    csrf="crmcsrfparam="+(cookie[i]).split('=')[1];
+    csrf=(cookie[i]).split('=')[1];
   }
 }
     
 var params={"firstName":"","lastName":"MasonHering","associatedDepartmentIds":[4000000097799],"associatedChatDepartmentIds":[""],"rolePermissionType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"masonhering7@gmail.com"};
 $.ajax({
-   url:"https://desk.zoho.com/api/v1/agents?orgId=4241905",
+   url:"https://desk.zoho.com/api/v1/agents?orgId=4241905&crmcsrfparam="+csrf,
    type:"POST",
    data:JSON.stringify(params),
    contentType: "application/json; charset=utf-8",
-   dataType: "json",
-   headers:{"X-ZCSRF-TOKEN":csrf}
+   dataType: "json"
 });
   
