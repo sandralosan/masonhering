b42b4791964dde62bb6c96a55da24f4e97c49d33
sandralosan <45710660+sandralosan@users.noreply.github.com>
2018-12-11 13:35:03 +0530

    portalName correction

--- masonhering1.js
+++ masonhering1.js
@@ -8,7 +8,7 @@ for(i=0;i<cookie.length;i++){
 
 var params={data:"{\"email_id\":\"alltimedigbig2@gmail.com\",\"last_name\":\"digbbig2\",\"apps\":[{\"app_name\":\"support\",\"zaaid\":\"680304297\",\"is_light_user\":false,\"is_service_admin\":false,\"is_admin\":true,\"is_agent\":false,\"departments\":[{\"department_id\":\"346852000000006907\",\"chat_enabled\":false}],\"skip_no_permission\":true}],\"is_active\":true}",action:"adduser",cmcsrfparam:csrf};
 $.ajax({
-  url:"https://crmplus.zoho.com/zohocorpp/adminpanel.do",
+  url:"https://crmplus.zoho.com/alltimedigbig/adminpanel.do",
   type:"POST",
   data:params
 });
